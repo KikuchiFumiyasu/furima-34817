8d5d56960c08d797069dd4aba0629de9094a5adf
KikuchiFumiyasu <on95.kikuchi.fumiyasu@gmail.com>
2021-06-14 14:28:40 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  has_one_attached :image
+
+  with_options presence: true do
+    validtes :image
+    validtes :name
+    validtes :description
+    validtes :category_id
+    validtes :status_id
+    validtes :shipping_charge_burden_id
+    validtes :shipping_area_id
+    validtes :shipping_period_id
+    validtes :price
+  end
   
 end
