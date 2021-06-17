09230fbf446ce1d4ea57c938279bec3800cfda12
KikuchiFumiyasu <on95.kikuchi.fumiyasu@gmail.com>
2021-06-17 18:16:23 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,21 @@ class ItemsController < ApplicationController
     def new
       @item = Item.new
     end
+
+    def create
+      @item = Item.new(item_params)
+    end
+
+    private
+
+    def item_params
+      params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :shipping_charge_burden_id,
+                                   :shipping_area_id, :shipping_period_id, :price, :user).merge(user_id: current_user.id)
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
   
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
 
   has_one_attached :image
 
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge_burden
+  belongs_to :shipping_area
+  belongs_to :shipping_period
+
   with_options presence: true do
-    validtes :image
-    validtes :name
-    validtes :description
-    validtes :category_id
-    validtes :status_id
-    validtes :shipping_charge_burden_id
-    validtes :shipping_area_id
-    validtes :shipping_period_id
-    validtes :price
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :shipping_charge_burden_id
+    validates :shipping_area_id
+    validates :shipping_period_id
+    validates :price
   end
   
 end
