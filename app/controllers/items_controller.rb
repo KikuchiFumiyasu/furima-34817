5198b72d20daea4688e8093dde9beca8fd82729b
KikuchiFumiyasu <on95.kikuchi.fumiyasu@gmail.com>
2021-06-29 19:54:39 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :aggregate_items, only: [:show, :edit, :update]
+  before_action :aggregate_items, only: [:show, :edit, :update, :destroy]
   before_action :appropriate_user, only: [:edit, :update]
 
   def index
@@ -26,6 +26,14 @@ class ItemsController < ApplicationController
       redirect_to item_path(@item)
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to item_path
     end
   end
 
