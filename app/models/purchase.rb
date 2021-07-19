e300635b56510f66a38046c1d68cd79a78ac7e3c
KikuchiFumiyasu <on95.kikuchi.fumiyasu@gmail.com>
2021-07-19 09:09:13 +0900

    Formオブジェクトの実装

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,3 @@
 class Purchase < ApplicationRecord
+  has_one :address
 end
---
+++ app/models/purchase_address.rb
@@ -0,0 +1,19 @@
+class PurchaseAddress
+
+  include ActiveModel::Model
+  attr_accessor :post_code, :shipping_area_id, :city, :address, :building_name, :phone_number, :purchase, :item_id, :user_id
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :shipping_area_id, numericality: { other_than: 1 }
+    validates :city
+    validates :address
+    VALID_PHONE_NUMBER_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
+    validates :phone_number, numericality: { with: VALID_PHONE_NUMBER_REGEX, only_integer: true }, length: { maximum: 11 }
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, shipping_area_id: shipping_area_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
